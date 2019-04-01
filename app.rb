e22521f4966d8394a05378b81d05f6e339008a68
hoffm386 <erinrhof@uw.edu>
2019-04-01 15:11:12 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    'Hello World'
+  end
+
+  get '/name' do
+    "My name is Erin"
+  end
+
+  get '/hometown' do
+    "My hometown is Lansing"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Bubbletoes'"
+  end
 end

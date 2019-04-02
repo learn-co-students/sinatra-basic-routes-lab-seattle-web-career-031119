723a706392da639b948998457b3c3c1637740560
ConditionalStatementLifestyle <github email address>
2019-04-02 10:12:32 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    'Hello, World!'
+  end
+  get '/name' do
+    'My name is Jon'
+  end
+  get '/hometown' do
+    'My hometown is Neenah'
+  end
+  get '/favorite-song' do
+    'Favorite song is Life\'s a Bitch'
+  end
+
+
 end

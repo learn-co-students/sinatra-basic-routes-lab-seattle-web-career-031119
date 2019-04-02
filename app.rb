c58a99f8ba4db32b56bff4287d22f722271844b0
256hz <a.d@256hz.com>
2019-04-02 09:35:44 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Abe."
+  end
+
+  get '/hometown' do
+    "My hometown is Havertown, PA."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is good."
+  end
+
 end
